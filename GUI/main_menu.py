3eeca95e847ceb5ef40b49e7b92031eb068bfc7f
Mateo Delgado <47906328+doGfloW@users.noreply.github.com>
2022-02-03 17:14:45 -0800

    Update Main menu

---
+++ GUI/main_menu.py
@@ -0,0 +1,10 @@
+# this is the main menu window, this is the start of the entire program.
+# it allows the user to navigate to the test windows and live session windows.
+# the user can decide to read files, simulate and livestream data from openbci hardware.
+# the user can select the hardware they want to interface with: arduino, robot, simulation.
+
+
+# Libraries used for main menu
+
+import sys
+from PyQt5 import QtGui
