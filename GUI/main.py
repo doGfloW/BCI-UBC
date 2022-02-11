2bcd549b83ca2207b61ba20ec024963d23a0ffb0
Mateo Delgado <47906328+doGfloW@users.noreply.github.com>
2022-02-10 17:12:00 -0800

    Working on Graph Window Jabroni

---
+++ GUI/main.py
@@ -0,0 +1,246 @@
+import argparse
+import logging
+
+
+import sys
+from PyQt5 import QtGui
+from PyQt5.QtOpenGL import *
+from PyQt5 import QtCore, Qt
+from PyQt5.Qt import *
+from PyQt5.QtWidgets import *
+import pyqtgraph as pg
+from pyqtgraph.Qt import QtGui, QtCore
+from PyQt5.QtGui import QFont
+from brainflow.board_shim import BoardShim, BrainFlowInputParams, BoardIds
+from brainflow.data_filter import DataFilter, FilterTypes, DetrendOperations
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
+import matplotlib.pyplot as plt
+import random
+class Graph:
+    def __init__(self, board_shim):
+        self.board_id = board_shim.get_board_id()
+        self.board_shim = board_shim
+        self.exg_channels = BoardShim.get_exg_channels(self.board_id)
+        self.sampling_rate = BoardShim.get_sampling_rate(self.board_id)
+        self.update_speed_ms = 50
+        self.window_size = 4
+        self.num_points = self.window_size * self.sampling_rate
+        self.app = QtGui.QApplication([])
+        self.win = pg.GraphicsWindow(title='BrainFlow Plot',size=(800, 600))
+
+        self._init_timeseries()
+
+        timer = QtCore.QTimer()
+        timer.timeout.connect(self.update)
+        timer.start(self.update_speed_ms)
+        QtGui.QApplication.instance().exec_()
+
+
+    def _init_timeseries(self):
+        self.plots = list()
+        self.curves = list()
+        for i in range(len(self.exg_channels)):
+            p = self.figure.addPlot(row=i,col=0)
+            p.showAxis('left', False)
+            p.setMenuEnabled('left', False)
+            p.showAxis('bottom', False)
+            p.setMenuEnabled('bottom', False)
+            if i == 0:
+                p.setTitle('TimeSeries Plot')
+            self.plots.append(p)
+            curve = p.plot()
+            self.curves.append(curve)
+
+    def update(self):
+        data = self.board_shim.get_current_board_data(self.num_points)
+        for count, channel in enumerate(self.exg_channels):
+            # plot timeseries
+            DataFilter.detrend(data[channel], DetrendOperations.CONSTANT.value)
+            DataFilter.perform_bandpass(data[channel], self.sampling_rate, 51.0, 100.0, 2,
+                                        FilterTypes.BUTTERWORTH.value, 0)
+            DataFilter.perform_bandpass(data[channel], self.sampling_rate, 51.0, 100.0, 2,
+                                        FilterTypes.BUTTERWORTH.value, 0)
+            DataFilter.perform_bandstop(data[channel], self.sampling_rate, 50.0, 4.0, 2,
+                                        FilterTypes.BUTTERWORTH.value, 0)
+            DataFilter.perform_bandstop(data[channel], self.sampling_rate, 60.0, 4.0, 2,
+                                        FilterTypes.BUTTERWORTH.value, 0)
+            self.curves[count].setData(data[channel].tolist())
+
+        self.app.processEvents()
+
+
+def main():
+    BoardShim.enable_dev_board_logger()
+    logging.basicConfig(level=logging.DEBUG)
+
+    parser = argparse.ArgumentParser()
+    # use docs to check which parameters are required for specific board, e.g. for Cyton - set serial port
+    parser.add_argument('--timeout', type=int, help='timeout for device discovery or connection', required=False,
+                        default=0)
+    parser.add_argument('--ip-port', type=int, help='ip port', required=False, default=0)
+    parser.add_argument('--ip-protocol', type=int, help='ip protocol, check IpProtocolType enum', required=False,
+                        default=0)
+    parser.add_argument('--ip-address', type=str, help='ip address', required=False, default='')
+    parser.add_argument('--serial-port', type=str, help='serial port', required=False, default='')
+    parser.add_argument('--mac-address', type=str, help='mac address', required=False, default='')
+    parser.add_argument('--other-info', type=str, help='other info', required=False, default='')
+    parser.add_argument('--streamer-params', type=str, help='streamer params', required=False, default='')
+    parser.add_argument('--serial-number', type=str, help='serial number', required=False, default='')
+    parser.add_argument('--board-id', type=int, help='board id, check docs to get a list of supported boards',
+                        required=False, default=BoardIds.SYNTHETIC_BOARD)
+    parser.add_argument('--file', type=str, help='file', required=False, default='')
+    args = parser.parse_args()
+
+    params = BrainFlowInputParams()
+    params.ip_port = args.ip_port
+    params.serial_port = args.serial_port
+    params.mac_address = args.mac_address
+    params.other_info = args.other_info
+    params.serial_number = args.serial_number
+    params.ip_address = args.ip_address
+    params.ip_protocol = args.ip_protocol
+    params.timeout = args.timeout
+    params.file = args.file
+
+    try:
+        board_shim = BoardShim(args.board_id, params)
+        board_shim.prepare_session()
+        board_shim.start_stream(450000, args.streamer_params)
+        #Graph(board_shim)
+        app = QApplication(sys.argv)    
+        win = MainWindow(board_shim) 
+        win.show() 
+        # print('we got here')  
+        sys.exit(app.exec())
+    except BaseException:
+        logging.warning('Exception', exc_info=True)
+    finally:
+        logging.info('End')
+        if board_shim.is_prepared():
+            logging.info('Releasing session')
+            board_shim.release_session()
+
+#Creating a Window
+class MainWindow(QMainWindow):
+    def __init__(self,board_shim):
+        super().__init__()
+        self.board_id = board_shim.get_board_id()
+        self.board_shim = board_shim
+        self.exg_channels = BoardShim.get_exg_channels(self.board_id)
+        self.sampling_rate = BoardShim.get_sampling_rate(self.board_id)
+        self.update_speed_ms = 50
+        self.window_size = 4
+        self.num_points = self.window_size * self.sampling_rate
+        self.plots = list()
+        self.curves = list()
+
+
+        
+
+        # a figure instance to plot on
+        self.figure = plt.figure()
+  
+        # this is the Canvas Widget that
+        # displays the 'figure'it takes the
+        # 'figure' instance as a parameter to __init__
+        self.canvas = FigureCanvas(self.figure)
+  
+        # this is the Navigation widget
+        # it takes the Canvas widget and a parent
+        self.toolbar = NavigationToolbar(self.canvas, self)
+        #Set the window title, size, and make an icon
+        self.setWindowTitle('BigBrain Data Channels')
+        self.resize(500, 500)
+
+        #Additional Imports for Testing
+       
+        self.layout0 = QVBoxLayout()
+
+        #Create central widget, apply the main layout, and set it as the central widget
+        self.widget = QWidget()
+        self.widget.setLayout(self.layout0)
+        self.setCentralWidget(self.widget)
+        self.layout0.addWidget(self.toolbar)
+		
+		# adding canvas to the layout
+        self.layout0.addWidget(self.canvas)
+
+        self.label1 = QLabel()
+        
+
+       
+        #Change the font jabroni
+        #self.label1.setFont(QFont('Times New Roman', 20))
+        self.label1.setAlignment(Qt.AlignCenter)
+
+        #Make a QLabel and a QPushButton
+        self.button1 = QPushButton("Channel 1")
+        self.button2 = QPushButton("Channel 2")
+        self.button3 = QPushButton( "Channel 3")
+        self.button4 = QPushButton( "Channel 4")
+        #Make a horizontal jabroni layout and add them to it
+        self.layout1 = QHBoxLayout()
+        self.layout1.addWidget(self.button1)
+        self.layout1.addWidget(self.button2)
+        
+
+        #Add layout1 to layout0
+        self.layout0.addLayout(self.layout1)
+        #When the button is clicked, connect to the function in the ()
+        #self.button1.clicked.connect()
+        #For simple statements, we use lambda:
+        #self.button1.clicked.connect( print("button1”))
+        #self.button2.clicked.connect( print("button2"))
+
+        #connect button2 to a function
+        self.button2.clicked.connect(self.message)
+        #define the function
+    def message(self):
+        self._init_timeseries()
+        timer = QtCore.QTimer()
+        timer.timeout.connect(self.update)
+        timer.start(self.update_speed_ms)
+        QtGui.QApplication.instance().exec_()
+
+    def _init_timeseries(self):
+        # random data
+        data = self.curves
+  
+        # clearing old figure
+        self.figure.clear()
+  
+        # create an axis
+        ax = self.figure.add_subplot(111)
+  
+        # plot data
+        ax.plot(data, '*-')
+  
+        # refresh canvas
+        self.canvas.draw()
+
+    def update(self):
+        data = self.board_shim.get_current_board_data(self.num_points)
+     
+        # plot timeseries
+        DataFilter.detrend(data[1], DetrendOperations.CONSTANT.value)
+        DataFilter.perform_bandpass(data[1], self.sampling_rate, 51.0, 100.0, 2,
+                                    FilterTypes.BUTTERWORTH.value, 0)
+        DataFilter.perform_bandpass(data[1], self.sampling_rate, 51.0, 100.0, 2,
+                                    FilterTypes.BUTTERWORTH.value, 0)
+        DataFilter.perform_bandstop(data[1], self.sampling_rate, 50.0, 4.0, 2,
+                                    FilterTypes.BUTTERWORTH.value, 0)
+        DataFilter.perform_bandstop(data[1], self.sampling_rate, 60.0, 4.0, 2,
+                                    FilterTypes.BUTTERWORTH.value, 0)
+        self.curves[1].setData(data[1].tolist())
+
+        self.app.processEvents()
+
+
+
+
+
+
+if __name__ == '__main__':
+    
+    main()
