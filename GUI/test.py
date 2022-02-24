bc3669912c0f9342b2468bd95fe55ec142c12a26
alexisgg08 <75453174+alexisgg08@users.noreply.github.com>
2022-02-24 15:01:42 -0800

    changed titles and update function

--- GUI/test.py
+++ GUI/test.py
@@ -15,9 +15,9 @@ class MainWindow(QMainWindow):
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
 
-        styles = {'color': 'r', 'font-size': '16px'}
+        styles = {'color': 'black', 'font-size': '16px'}
         self.setWindowTitle('Main Window')
-        self.setGeometry(0, 0, 1200, 900)
+        self.setGeometry(0, 0, 1500, 900)
 
         self.vLayout = QVBoxLayout()
         self.hLayout = QHBoxLayout()
@@ -61,25 +61,29 @@ class MainWindow(QMainWindow):
         self.hLayout.addWidget(self.button3)
         self.hLayout.addWidget(self.button4)
 
-        self.graphWidget = pg.PlotWidget()
-        self.graphWidget.setLabel('left', 'Amplitude', **styles)
-        self.graphWidget.setLabel('bottom', 'Time', **styles)
-        self.graphWidget.setBackground('w')
-        self.gridLayout.addWidget(self.graphWidget, 0, 0)
+        self.graphWidget1 = pg.PlotWidget()
+        self.graphWidget1.setTitle('Channel 1', **styles)
+        self.graphWidget1.setLabel('left', 'Amplitude', **styles)
+        self.graphWidget1.setLabel('bottom', 'Time', **styles)
+        self.graphWidget1.setBackground('w')
+        self.gridLayout.addWidget(self.graphWidget1, 0, 0)
 
         self.graphWidget2 = pg.PlotWidget()
+        self.graphWidget2.setTitle('Channel 2', **styles)
         self.graphWidget2.setLabel('left', 'Amplitude', **styles)
         self.graphWidget2.setLabel('bottom', 'Time', **styles)
         self.graphWidget2.setBackground('w')
         self.gridLayout.addWidget(self.graphWidget2, 0, 1)
 
         self.graphWidget3 = pg.PlotWidget()
+        self.graphWidget3.setTitle('Channel 3', **styles)
         self.graphWidget3.setLabel('left', 'Amplitude', **styles)
         self.graphWidget3.setLabel('bottom', 'Time', **styles)
         self.graphWidget3.setBackground('w')
         self.gridLayout.addWidget(self.graphWidget3, 1, 0)
 
         self.graphWidget4 = pg.PlotWidget()
+        self.graphWidget4.setTitle('Channel 4', **styles)
         self.graphWidget4.setLabel('left', 'Amplitude', **styles)
         self.graphWidget4.setLabel('bottom', 'Time', **styles)
         self.graphWidget4.setBackground('w')
@@ -89,7 +93,7 @@ class MainWindow(QMainWindow):
         self.y = [randint(0, 100) for _ in range(100)]  # 100 data points
 
         pen = pg.mkPen(color=(255, 0, 0))
-        self.data_line1 = self.graphWidget.plot(self.x, self.y, pen=pen)
+        self.data_line1 = self.graphWidget1.plot(self.x, self.y, pen=pen)
         self.data_line2 = self.graphWidget2.plot(self.x, self.y, pen=pen)
         self.data_line3 = self.graphWidget3.plot(self.x, self.y, pen=pen)
         self.data_line4 = self.graphWidget4.plot(self.x, self.y, pen=pen)
@@ -106,21 +110,21 @@ class MainWindow(QMainWindow):
             self.y = self.y[1:]  # Remove the first
             self.y.append(randint(0, 100))  # Add a new random value.
             self.data_line1.setData(self.x, self.y)  # Update the data.
-            
+
         if self.button2.isChecked():
             self.x = self.x[1:]  # Remove the first y element.
             self.x.append(self.x[-1] + 1)  # Add a new value 1 higher than the last.
             self.y = self.y[1:]  # Remove the first
             self.y.append(randint(0, 100))  # Add a new random value.
             self.data_line2.setData(self.x, self.y)  # Update the data.
-            
+
         if self.button3.isChecked():
             self.x = self.x[1:]  # Remove the first y element.
             self.x.append(self.x[-1] + 1)  # Add a new value 1 higher than the last.
             self.y = self.y[1:]  # Remove the first
             self.y.append(randint(0, 100))  # Add a new random value.
             self.data_line3.setData(self.x, self.y)  # Update the data.
-            
+
         if self.button4.isChecked():
             self.x = self.x[1:]  # Remove the first y element.
             self.x.append(self.x[-1] + 1)  # Add a new value 1 higher than the last.
