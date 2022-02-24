b5c8df3e864eb3d0dd18940b9e97f9dbaf5e0aa5
alexisgg08 <75453174+alexisgg08@users.noreply.github.com>
2022-02-24 14:04:42 -0800

    added graphs and changed buttons

--- GUI/test.py
+++ GUI/test.py
@@ -1,4 +1,3 @@
-
 from pyqtgraph import PlotWidget, plot
 import pyqtgraph as pg
 import sys  # We need sys so that we can pass argv to QApplication
@@ -7,60 +6,120 @@ from random import randint
 from PyQt5 import QtGui
 from PyQt5.QtOpenGL import *
 from PyQt5 import QtCore, Qt
-from PyQt5.QtCore import Qt
-
 from PyQt5.QtWidgets import *
 from PyQt5 import QtWidgets
-       
+
 
 class MainWindow(QMainWindow):
 
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
 
-        
-        self.layout0 = QVBoxLayout()
+        styles = {'color': 'r', 'font-size': '16px'}
+        self.setWindowTitle('Main Window')
+        self.setGeometry(0, 0, 1200, 900)
+
+        self.vLayout = QVBoxLayout()
+        self.hLayout = QHBoxLayout()
+        self.gridLayout = QGridLayout()
+        self.vLayout.addLayout(self.hLayout)
+        self.vLayout.addLayout(self.gridLayout)
+
+        self.vLayout.setContentsMargins(10, 10, 10, 10)
+        self.vLayout.setSpacing(10)
+        self.hLayout.setContentsMargins(10, 10, 10, 10)
+        self.hLayout.setSpacing(10)
+
         self.widget = QWidget()
+        self.widget.setLayout(self.vLayout)
+        self.setCentralWidget(self.widget)
+
+        self.button1 = QPushButton('Channel 1')
+        self.button2 = QPushButton('Channel 2')
+        self.button3 = QPushButton('Channel 3')
+        self.button4 = QPushButton('Channel 4')
+        self.button1.setFixedSize(80, 30)
+        self.button2.setFixedSize(80, 30)
+        self.button3.setFixedSize(80, 30)
+        self.button4.setFixedSize(80, 30)
+
+        self.button1.setCheckable(True)
+        self.button2.setCheckable(True)
+        self.button3.setCheckable(True)
+        self.button4.setCheckable(True)
+        self.button1.clicked.connect(self.change_colour)
+        self.button2.clicked.connect(self.change_colour)
+        self.button3.clicked.connect(self.change_colour)
+        self.button4.clicked.connect(self.change_colour)
+        # self.button1.clicked.connect(self.update_plot_data)
+        # self.button2.clicked.connect(self.update_plot_data)
+        # self.button3.clicked.connect(self.update_plot_data)
+        # self.button4.clicked.connect(self.update_plot_data)
+        self.button1.setStyleSheet("background-color : lightgrey")
+        self.button2.setStyleSheet("background-color : lightgrey")
+        self.button3.setStyleSheet("background-color : lightgrey")
+        self.button4.setStyleSheet("background-color : lightgrey")
+
+        self.hLayout.addWidget(self.button1)
+        self.hLayout.addWidget(self.button2)
+        self.hLayout.addWidget(self.button3)
+        self.hLayout.addWidget(self.button4)
+
         self.graphWidget = pg.PlotWidget()
-        self.setCentralWidget(self.graphWidget)
-        self.graphWidget.setLayout(self.layout0)
-        self.button1 = QPushButton("Channel 1")
-        self.button1.setFixedSize(120, 30) 
-        self.button2 = QPushButton("Channel 2")
-        self.button3 = QPushButton( "Channel 3")
-        self.button4 = QPushButton( "Channel 4")
-        self.layout0.addWidget(self.button1)
-
-    
-        self.x = list(range(100))  # 100 time points
-        self.y = [randint(0,100) for _ in range(100)]  # 100 data points
-
+        self.graphWidget.setLabel('left', 'Amplitude', **styles)
+        self.graphWidget.setLabel('bottom', 'Time', **styles)
         self.graphWidget.setBackground('w')
+        self.gridLayout.addWidget(self.graphWidget, 0, 0)
+
+        self.graphWidget2 = pg.PlotWidget()
+        self.graphWidget2.setLabel('left', 'Amplitude', **styles)
+        self.graphWidget2.setLabel('bottom', 'Time', **styles)
+        self.graphWidget2.setBackground('w')
+        self.gridLayout.addWidget(self.graphWidget2, 0, 1)
+
+        self.graphWidget3 = pg.PlotWidget()
+        self.graphWidget3.setLabel('left', 'Amplitude', **styles)
+        self.graphWidget3.setLabel('bottom', 'Time', **styles)
+        self.graphWidget3.setBackground('w')
+        self.gridLayout.addWidget(self.graphWidget3, 1, 0)
+
+        self.graphWidget4 = pg.PlotWidget()
+        self.graphWidget4.setLabel('left', 'Amplitude', **styles)
+        self.graphWidget4.setLabel('bottom', 'Time', **styles)
+        self.graphWidget4.setBackground('w')
+        self.gridLayout.addWidget(self.graphWidget4, 1, 1)
+
+        self.x = list(range(100))  # 100 time points
+        self.y = [randint(0, 100) for _ in range(100)]  # 100 data points
 
         pen = pg.mkPen(color=(255, 0, 0))
-        self.data_line =  self.graphWidget.plot(self.x, self.y, pen=pen)
+        self.data_line = self.graphWidget.plot(self.x, self.y, pen=pen)
+        self.data_line2 = self.graphWidget2.plot(self.x, self.y, pen=pen)
+        self.data_line3 = self.graphWidget3.plot(self.x, self.y, pen=pen)
+        self.data_line4 = self.graphWidget4.plot(self.x, self.y, pen=pen)
+
         self.timer = QtCore.QTimer()
         self.timer.setInterval(50)
         self.timer.timeout.connect(self.update_plot_data)
         self.timer.start()
 
-        styles = {'color':'r', 'font-size':'20px'}
-        self.graphWidget.setLabel('left', 'Amplitude', **styles)
-        self.graphWidget.setLabel('bottom', 'x', **styles)
-
-
-       
-
-
     def update_plot_data(self):
-
         self.x = self.x[1:]  # Remove the first y element.
         self.x.append(self.x[-1] + 1)  # Add a new value 1 higher than the last.
-
         self.y = self.y[1:]  # Remove the first
-        self.y.append( randint(0,100))  # Add a new random value.
+        self.y.append(randint(0, 100))  # Add a new random value.
 
         self.data_line.setData(self.x, self.y)  # Update the data.
+        self.data_line2.setData(self.x, self.y)  # Update the data.
+        self.data_line3.setData(self.x, self.y)  # Update the data.
+        self.data_line4.setData(self.x, self.y)  # Update the data.
+
+    def change_colour(self):
+        if self.button.isChecked():
+            self.button.setStyleSheet("background-color : lightblue")
+        else:
+            self.button.setStyleSheet("background-color : lightgrey")
+
 
 def main():
     app = QtWidgets.QApplication(sys.argv)
@@ -68,6 +127,6 @@ def main():
     main.show()
     sys.exit(app.exec_())
 
+
 if __name__ == '__main__':
     main()
-
