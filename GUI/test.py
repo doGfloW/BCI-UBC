f55bfe954feff75a0cfd1a9cffbdc3615f0d0175
Mateo Delgado <47906328+doGfloW@users.noreply.github.com>
2022-03-08 17:32:39 -0800

    update to main
    baseline attention and live testing

--- GUI/test.py
+++ GUI/test.py
@@ -1,1 +1,12 @@
-#This is our Motor Imagery Test - Version 1
+#test new models
+
+from liblsl import StreamInlet, resolve_stream
+from pydoc import resolve
+import numpy as np
+import time
+import matplotlib
+import sys
+from collections import deque
+
+print("looking for streams...")
+streams = resolve
