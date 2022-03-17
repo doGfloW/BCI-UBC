6abf9e6ba13f6c3d3e29dd5f6560fbdf68aef8c8
Alexis <alynan08@gmail.com>
2022-03-17 13:35:17 -0700

    bandpower function for python execution

--- GUI/pythontomat.py
+++ GUI/pythontomat.py
@@ -1,4 +1,6 @@
 import matlab.engine
 m = matlab.engine.start_matlab()
-x = m.sqrt(42.0)
-print(x)
+data_fold = r'C:\Users\alyna\Documents\ENGR 499\GitHub Repo\Baseline_tests\Mateo_2sec'
+ex_fold = r'C:\Users\alyna\Documents\ENGR 499\GitHub Repo\Datasets'
+output = m.bandpower_extraction(data_fold, ex_fold)
+print(output)
