d3babdfedb6a64ac14ed66668ba66ae7b84f3088
Alexis <alynan08@gmail.com>
2022-03-17 13:36:04 -0700

    delete unnecessary py file

--- Matlab/pythontomat.py
+++
@@ -1,6 +0,0 @@
-import matlab.engine
-m = matlab.engine.start_matlab()
-data_fold = r'C:\Users\alyna\Documents\ENGR 499\GitHub Repo\Baseline_tests\Mateo_2sec'
-ex_fold = r'C:\Users\alyna\Documents\ENGR 499\GitHub Repo\Datasets'
-output = m.bandpower_extraction(data_fold, ex_fold)
-print(output)
