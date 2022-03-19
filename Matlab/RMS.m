795b98422018688740d772feef55a4d9e2839e57
Ethan Jennings <98867495+Somadman@users.noreply.github.com>
2022-03-19 16:12:52 -0700

    Update RMS.m

--- Matlab/RMS.m
+++ Matlab/RMS.m
@@ -1,78 +1,41 @@
 function [output] = bandpower_extraction(data_folder)
-    myDir = convertCharsToStrings(data_folder);
+    %inissilize varables 
     alldata=[];
-    myDir = uigetdir
-    myFiles = dir(fullfile(myDir,'*.txt')); %gets all txt files in struct
-    for k = 1:length(myFiles)
-        baseFileName = myFiles(k).name;
-        fullFileName = fullfile(myDir, baseFileName);
-        try
-            data = dlmread(fullFileName, '\t');
-        catch
-            data = dlmread(fullFileName, ',');
+    a_channel= [];
+    b_channel= [];
+    output=[];
+    myDir = convertCharsToStrings(data_folder);
+    try
+        data = dlmread(data_folder, '\t');
+    catch
+        data = dlmread(data_folder, ',');
+    end
+    [data_row,data_col]=size(data); % passing variabe to matixe size
+
+    for channel=2:(data_col-2)
+        EEG = data(:,channel);
+        if sum(EEG)==0 % checking if the row is empty
+            continue
         end
-        r_channel= [];
-        fprintf(1, 'Now reading %s\n', fullFileName);
-        markers=data(:,end)*1E-5;
-        [data_row,data_col]=size(data);
-        for channel=2:(data_col-2)
-            EEG = data(:,channel);
-            if sum(EEG)==0
-                continue
-            end
-            fprintf('Now reading channel %d\n', (channel-1));
-            apass= bandpass(EEG,[8 13], 200);
-            bpass= bandpass(EEG,[13 32], 200);
-            [pks,locs]=findpeaks(data(:,15));
-            marker=[locs,pks];
-            a_fft= fft(apass);
-            l=length(pks);
-            c=1;
-            T1=zeros(200,1);
-            T2=zeros(200,1);
-            %event={};
-            for count =1 : length(pks)
-                if pks(count)<=2
-                    T1(c)=locs(count);
-                    event(c,1)="movement";
-                    if pks(count)==2
-                        event(c,1)="still";
-                    end
-                    c=c+1;
-                end
-            end
-            c=1;
-            for count =1 : length(pks)
-                if pks(count)>10
-                    T2(c)=locs(count);
-                    c=c+1;
-                end
-            end
-            T1=nonzeros(T1);
-            T2=nonzeros(T2);
-            %event=nonzeros(event);
-            M=[T1,T2]; 
-            abandpower=zeros(20,1);
-            bbandpower=zeros(20,1);
-            r=zeros(1,20);
-            for k = 1:length(M) % indices
-                if M(k)==0
-                    break
-                end
-                i=M(k,1);
-                j=M(k,2);
-                r(k)=rms(apass(i:j));
-                %bbandpower(k)=bandpower(bpass(i:j));
-                %r(k)=abandpower(k)/bbandpower(k);
-            end
-    %         abandpower=nonzeros(abandpower);
-    %         bbandpower=nonzeros(bbandpower);
-             r=nonzeros(r);
-            temp=r_channel;
-            r_channel=[temp,r];
+        % filtering for alpha and beta
+        apass= bandpass(EEG,[8 13], 200);
+        bpass= bandpass(EEG,[13 32], 200);
+        alpha_rms=zeros(1,20);
+        beta=zeros(1,20);
+        alpha_rms=rms(apass);
+        beta_rms=rms(bpass);
+
         end
-        bp=[r_channel,event];
+        % adds the rms variabel and move to next channel
+        alpha_rms=nonzeros(alpha_rms);
+        beta_rms=nonzeros(beta_rms);
+        temp_a=a_channel;
+        temp_b=b_channel;
+        a_channel=[temp_a,alpha_rms];
+        b_channel=[temp_b,beta_rms];
+    end
+    bp=[a_channel,b_channel];
     %     
     alldata=[alldata;bp];
     end
-    output=alldata;
+    output=alldata; %returns the feature extreation
