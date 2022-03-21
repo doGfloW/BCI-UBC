ec781dac874b6c8a6c696f75801906cf9fe3cdf8
Alexis <alynan08@gmail.com>
2022-03-21 16:15:47 -0700

    data_txtfile name change

--- GUI/pythontomat.py
+++ GUI/pythontomat.py
@@ -1,6 +1,6 @@
 import matlab.engine
 m = matlab.engine.start_matlab()
-data_txtfile = r"C:\Users\alyna\Documents\ENGR 499\BCI-UBC\Matlab\mateo_2sec_for_testing.txt"
+data_txtfile = r"mateo_2sec_for_testing.txt"
 output = m.bandpower_extraction(data_txtfile)
 output = list(output[0])
 print(output)
