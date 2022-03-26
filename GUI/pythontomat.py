cb3e5998c43ddb00a791f1a6dca68cdb073cdcdb
Alexis <alynan08@gmail.com>
2022-03-26 16:29:55 -0700

    matlab changes

--- GUI/pythontomat.py
+++ GUI/pythontomat.py
@@ -1,6 +1,10 @@
 import matlab.engine
+import pandas as pd
+import sys
+
 m = matlab.engine.start_matlab()
 data_txtfile = r"mateo_2sec_for_testing.txt"
+
 bp_vals, rms_vals = m.bp_rms_extraction(data_txtfile, nargout=2)
 bp_vals = list(bp_vals[0])
 rms_vals = list(rms_vals[0])
