cf0b5eeea046870de1a0500d59ea333bdf876f57
Mateo <mateod98@gmail.com>
2022-03-28 11:27:39 -0700

    new data changes

--- Matlab/preprocessing.m
+++ Matlab/preprocessing.m
@@ -1,5 +1,5 @@
 function preprocessing
-    dirty_cyton_data=readmatrix('mateo_2sec_for_testing.txt'); %File type here could be a csv,txt or xlsx file
+    dirty_cyton_data=readmatrix('dirty_cyton_data.txt'); %File type here could be a csv,txt or xlsx file
 %Raw Data coming from cyton has the following columns:
     time_data=dirty_cyton_data(:,1);
     attention_channel_ch1=dirty_cyton_data(:,2);
@@ -10,7 +10,6 @@ function preprocessing
     dirty_cyton_data_ch6=dirty_cyton_data(:,7);
     dirty_cyton_data_ch7=dirty_cyton_data(:,8);
     reference_channel_ch8=dirty_cyton_data(:,9);
-
 %Re-referencing all channels except the attention channel:
     clean_cyton_data_ch2=dirty_cyton_data_ch2-reference_channel_ch8;
     clean_cyton_data_ch3=dirty_cyton_data_ch3-reference_channel_ch8;
@@ -20,17 +19,18 @@ function preprocessing
     clean_cyton_data_ch7=dirty_cyton_data_ch7-reference_channel_ch8;
 
 
-    preprocessing=[time_data attention_channel_ch1 clean_cyton_data_ch2 clean_cyton_data_ch3 clean_cyton_data_ch4 clean_cyton_data_ch5 clean_cyton_data_ch6 clean_cyton_data_ch7 reference_channel_ch8];
+    preprocessing=[time_data attention_channel_ch1 clean_cyton_data_ch2 clean_cyton_data_ch3 clean_cyton_data_ch4 clean_cyton_data_ch5 clean_cyton_data_ch6 clean_cyton_data_ch7 reference_channel_ch8 ];
     writematrix(preprocessing,'clean_cyton_data.csv')
 
 %Pass attention channel in preprocessed data to the bandpass filtering
-    EEG=clean_cyton_data_ch2
+    EEG=clean_cyton_data_ch7
     
-    apass= bandpass(EEG,[8 13], 200);
-    bpass= bandpass(EEG,[13 32], 200);
+    apass= bandpass(EEG,[8 13], 250);
+    bpass= bandpass(EEG,[13 32], 250);
     l=length(EEG); % finging lenth of data
     t=(0:l-1)*1/200; %calcuating time
-    markers=dirty_cyton_data(:,end)*1E-5;
+    markers=dirty_cyton_data(:,14);
+    markers
     %% plots
     close all
     tiledlayout(2,2)
