d9c7d51ea1615b23275f7cbec2158544034c95f2
alexisgg08 <75453174+alexisgg08@users.noreply.github.com>
2022-04-04 13:32:52 -0700

    Update EEG_filtering.m

--- Matlab/EEG_filtering.m
+++ Matlab/EEG_filtering.m
@@ -1,69 +1,69 @@
 clear all
-%close all
-%% loading data to be read
+% close all
+% read data file
 [file,path] = uigetfile('*.txt');
 try
     data = dlmread(file, '\t');
 catch
     data = dlmread(file, ',');
 end
-for x= 1:1
-    data(1,:) = [];
-    x=x+1;
-end
-emg = data (:,3);
-markers=data(:,end)*1E-5;
-EEG=emg*1E-6 + 8.5*1E-4 ;
-% filltering data
+
+emg = data(:, 3);
+markers = data(:, end)*1E-5;
+EEG = emg*1E-6 + 8.5*1E-4;
+
+% apply bandpass filter to the data (alpha and beta frequency bands)
 apass= bandpass(EEG,[8 13], 250);
 bpass= bandpass(EEG,[13 32], 250); 
-% for x= 1:350
-%     apass(1,:) = [];
-%     apass(end,:) = [];
-%     bpass(1,:) = [];
-%     bpass(end,:) = [];
-%     x=x+1;
-% end
-l=length(emg); % lenth of data
-t=(0:l-1)*1/250; %calcuating time
-%% plots
+
+% get length of data and calculate the time for each row
+l = length(emg);
+t = (0:l-1)*1/250;
+
+% create plots
 close all
 tiledlayout(2,2)
 nexttile
 hold on
-plot(t,EEG);
-plot(t,markers);
-title('EEG raw Data');
-xlabel('time (s)');
+
+% plot raw EEG data
+plot(t, EEG);
+plot(t, markers);
+title('Raw EEG Data');
+xlabel('Time (s)');
 ylabel('EEG Amplitude (uV)');
-legend("EEG","Marker");
+legend("EEG", "Marker");
 hold off
 nexttile
 hold on
-plot(apass,'r');
+
+% plot filtered alpha data
+plot(t, apass,'r');
 title('EEG Alpha Data');
-plot(markers);
-xlabel('time (s)');
+plot(t, markers);
+xlabel('Time (s)');
 ylabel('EEG Amplitude (uV)');
+legend("Alpha EEG", "Marker");
 hold off
 nexttile
 hold on
-plot(apass,'r');
-plot(bpass,'c');
-plot(markers);
+
+% plot filtered alpha and beta data
+plot(t, bpass,'c');
+plot(t, markers);
 title('EEG Beta Data');
-xlabel('time (s)');
+xlabel('Time (s)');
 ylabel('EEG Amplitude (uV)');
+legend("Beta EEG", "Marker");
 hold off
-%Frequency plot
+
+% FFT plot
 fs = 200;
 EEG_fft = fft(EEG);
 L = length(EEG_fft);
 f = (0:L-1)*fs/L;
 nexttile
-plot(f,abs(EEG_fft));
+plot(f, abs(EEG_fft));
 xlabel('Frequency (Hz)')
 ylabel('Amplitude')
-title('Frequency Data')
-
-
+title('FFT Plot')
