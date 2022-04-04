7aa0d96bae9d93ba00dbf03f21994f2d1cbe5c67
alexisgg08 <75453174+alexisgg08@users.noreply.github.com>
2022-04-04 13:33:48 -0700

    Update RMS_classification.m

--- Matlab/RMS_classification.m
+++ Matlab/RMS_classification.m
@@ -1,10 +1,8 @@
 function [output] = RMS_classification(VarName)
-    T=array2table(VarName); 
+    T = array2table(VarName); 
     allVars = 1:width(T);
-    newNames = append("VarName",string(allVars));
-    T = renamevars(T,allVars,newNames);
-%     myDir = convertCharsToStrings(VarName)
-%     inputTable=readtable(filename); 
+    newNames = append("VarName", string(allVars));
+    T = renamevars(T, allVars, newNames);
     load('rms_KNN_Model.mat')
     output = RMS_KNN_Model.predictFcn(T);
 end
