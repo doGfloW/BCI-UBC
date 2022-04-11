d946bb0771161d6ffbf6dc80c69d578905b0e931
Alexis <alynan08@gmail.com>
2022-04-11 11:58:22 -0700

    graph win cyton

--- GUI/live_graph_window.py
+++ GUI/live_graph_window.py
@@ -56,31 +56,55 @@ class graph_win(QMainWindow):
         self.button2 = QPushButton('Channel 2')
         self.button3 = QPushButton('Channel 3')
         self.button4 = QPushButton('Channel 4')
+        self.button5 = QPushButton('Channel 5')
+        self.button6 = QPushButton('Channel 6')
+        self.button7 = QPushButton('Channel 7')
+        self.button8 = QPushButton('Channel 8')
 
         self.button1.setFixedSize(80, 30)
         self.button2.setFixedSize(80, 30)
         self.button3.setFixedSize(80, 30)
         self.button4.setFixedSize(80, 30)
+        self.button5.setFixedSize(80, 30)
+        self.button6.setFixedSize(80, 30)
+        self.button7.setFixedSize(80, 30)
+        self.button8.setFixedSize(80, 30)
 
         self.button1.setCheckable(True)
         self.button2.setCheckable(True)
         self.button3.setCheckable(True)
         self.button4.setCheckable(True)
+        self.button5.setCheckable(True)
+        self.button6.setCheckable(True)
+        self.button7.setCheckable(True)
+        self.button8.setCheckable(True)
 
         self.button1.clicked.connect(self.update_plot_data)
         self.button2.clicked.connect(self.update_plot_data)
         self.button3.clicked.connect(self.update_plot_data)
         self.button4.clicked.connect(self.update_plot_data)
+        self.button5.clicked.connect(self.update_plot_data)
+        self.button6.clicked.connect(self.update_plot_data)
+        self.button7.clicked.connect(self.update_plot_data)
+        self.button8.clicked.connect(self.update_plot_data)
 
         self.button1.setStyleSheet("background-color : lightgrey")
         self.button2.setStyleSheet("background-color : lightgrey")
         self.button3.setStyleSheet("background-color : lightgrey")
         self.button4.setStyleSheet("background-color : lightgrey")
+        self.button5.setStyleSheet("background-color : lightgrey")
+        self.button6.setStyleSheet("background-color : lightgrey")
+        self.button7.setStyleSheet("background-color : lightgrey")
+        self.button8.setStyleSheet("background-color : lightgrey")
 
         self.hLayout.addWidget(self.button1)
         self.hLayout.addWidget(self.button2)
         self.hLayout.addWidget(self.button3)
         self.hLayout.addWidget(self.button4)
+        self.hLayout.addWidget(self.button5)
+        self.hLayout.addWidget(self.button6)
+        self.hLayout.addWidget(self.button7)
+        self.hLayout.addWidget(self.button8)
         #endregion
 
         #region Graphs
@@ -111,6 +135,34 @@ class graph_win(QMainWindow):
         self.graphWidget4.setLabel('bottom', 'Time', **styles)
         self.graphWidget4.setBackground('w')
         self.gridLayout.addWidget(self.graphWidget4, 3, 0)
+
+        self.graphWidget5 = pg.PlotWidget()
+        self.graphWidget5.setTitle('Channel 4', **styles)
+        self.graphWidget5.setLabel('left', 'Amplitude', **styles)
+        self.graphWidget5.setLabel('bottom', 'Time', **styles)
+        self.graphWidget5.setBackground('w')
+        self.gridLayout.addWidget(self.graphWidget5, 4, 0)
+
+        self.graphWidget6 = pg.PlotWidget()
+        self.graphWidget6.setTitle('Channel 4', **styles)
+        self.graphWidget6.setLabel('left', 'Amplitude', **styles)
+        self.graphWidget6.setLabel('bottom', 'Time', **styles)
+        self.graphWidget6.setBackground('w')
+        self.gridLayout.addWidget(self.graphWidget6, 5, 0)
+
+        self.graphWidget7 = pg.PlotWidget()
+        self.graphWidget7.setTitle('Channel 4', **styles)
+        self.graphWidget7.setLabel('left', 'Amplitude', **styles)
+        self.graphWidget7.setLabel('bottom', 'Time', **styles)
+        self.graphWidget7.setBackground('w')
+        self.gridLayout.addWidget(self.graphWidget7, 6, 0)
+
+        self.graphWidget8 = pg.PlotWidget()
+        self.graphWidget8.setTitle('Channel 4', **styles)
+        self.graphWidget8.setLabel('left', 'Amplitude', **styles)
+        self.graphWidget8.setLabel('bottom', 'Time', **styles)
+        self.graphWidget8.setBackground('w')
+        self.gridLayout.addWidget(self.graphWidget8, 7, 0)
         #endregion
 
         pen = pg.mkPen(color=(255, 0, 0))
@@ -120,6 +172,10 @@ class graph_win(QMainWindow):
         self.data_line2 = self.graphWidget2.plot(self.x, self.y, pen=pen)
         self.data_line3 = self.graphWidget3.plot(self.x, self.y, pen=pen)
         self.data_line4 = self.graphWidget4.plot(self.x, self.y, pen=pen)
+        self.data_line5 = self.graphWidget5.plot(self.x, self.y, pen=pen)
+        self.data_line6 = self.graphWidget6.plot(self.x, self.y, pen=pen)
+        self.data_line7 = self.graphWidget7.plot(self.x, self.y, pen=pen)
+        self.data_line8 = self.graphWidget8.plot(self.x, self.y, pen=pen)
 
         self.timer = QtCore.QTimer()
         self.timer.setInterval(100)
@@ -166,6 +222,38 @@ class graph_win(QMainWindow):
             self.y = data[self.eeg_channels[3]].tolist()
             self.data_line4.setData(self.y)
 
+        if self.button5.isChecked():
+            pass
+        else:
+            self.x = self.x[1:]
+            self.x.append(self.x[-1] + 1)
+            self.y = data[self.eeg_channels[4]].tolist()
+            self.data_line5.setData(self.y)
+
+        if self.button6.isChecked():
+            pass
+        else:
+            self.x = self.x[1:]
+            self.x.append(self.x[-1] + 1)
+            self.y = data[self.eeg_channels[5]].tolist()
+            self.data_line6.setData(self.y)
+
+        if self.button7.isChecked():
+            pass
+        else:
+            self.x = self.x[1:]
+            self.x.append(self.x[-1] + 1)
+            self.y = data[self.eeg_channels[6]].tolist()
+            self.data_line7.setData(self.y)
+
+        if self.button8.isChecked():
+            pass
+        else:
+            self.x = self.x[1:]
+            self.x.append(self.x[-1] + 1)
+            self.y = data[self.eeg_channels[7]].tolist()
+            self.data_line8.setData(self.y)
+
         # self.app.processEvents()
 
 
@@ -186,7 +274,7 @@ def main():
     parser.add_argument('--streamer-params', type=str, help='streamer params', required=False, default='')
     parser.add_argument('--serial-number', type=str, help='serial number', required=False, default='')
     parser.add_argument('--board-id', type=int, help='board id, check docs to get a list of supported boards',
-                        required=False, default=BoardIds.GANGLION_BOARD)
+                        required=False, default=BoardIds.CYTON_BOARD)
     parser.add_argument('--file', type=str, help='file', required=False, default='')
     args = parser.parse_args()
 
